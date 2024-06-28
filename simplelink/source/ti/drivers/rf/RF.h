597b8c008899af036edb2cc9ea497b5b9b8b2040
Karol Baryła <git@baryla.org>
2024-06-28 09:09:43 +0200

    add RF files

--- simplelink/source/ti/drivers/rf/RF.h
+++ simplelink/source/ti/drivers/rf/RF.h
@@ -50,6 +50,8 @@ is linked in and include this header file as follows:
 
 #if (DeviceFamily_PARENT == DeviceFamily_PARENT_CC13X2_CC26X2)
     #include <ti/drivers/rf/RFCC26X2.h>
+#elif (DeviceFamily_PARENT == DeviceFamily_PARENT_CC13X0_CC26X0)
+    #include <ti/drivers/rf/RFCC26XX.h>
 #else
 #error "An incompatible device is defined in the project. \
         To build with this driver, change the DeviceFamily token definition."
